83823a0227a2a098bfcb6c8015fd25a402b58bd4
Fumihiko Shiroyama <fu.shiroyama@gmail.com>
2014-01-22 02:41:07 +0000

    First commitment: rails new rails_mysql_template -d mysql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsMysqlTemplate::Application.config.secret_key_base = '3160654f1d29b72d8ca27cf59f906091f97e04b0a364028b1cf5224c7ef861fc2e29b50cc6ab54528cea60d1929e572b01324b89b5145b65053742bbca629506'
